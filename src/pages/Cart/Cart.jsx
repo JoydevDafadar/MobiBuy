32c53bc3bf6188d62b681484f552ee8e038506a2
Joydev <joydevdafadar2001@gmail.com>
2023-04-08 09:56:33 +0530

    cart section Implement

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,10 +1,74 @@
-import React from 'react'
+import React, { useState } from 'react';
+
+import "./Cart.css"
 import Navbar from '../../Components/Navbar/Navbar'
+import SmartPhones from '../../Utils/SmartPhones';
+import Accessories from '../../Utils/Accessories';
+
+
+// Retriving data from local sstorage
+const getLocalItem = () => {
+  let item = localStorage.getItem("Lists");
+  console.log( item );
+  if( item ){
+    return JSON.parse(localStorage.getItem("Lists"));
+  }
+  else{
+    return [];
+  }
+}
 
 const Cart = () => {
+
+  const [list, setList ] = useState(getLocalItem());
+  // const [total, setTotal ] = useState(0);
+
+  const delAllItem = () =>{
+    localStorage.setItem("Lists", JSON.stringify([]));
+    setList([]);
+  }
+
   return (
     <>
     <Navbar/>
+
+    <div className="cart p1 m1">
+      <hr/>
+      {
+        list.map( (ele, ind) => {
+
+
+          const id = parseInt(ele);
+
+          var cheak = id%10;
+          var id_num = parseInt(id/10);
+          // console.log( cheak, id_num ); 
+
+          // const T_price = (cheak === 0) ?SmartPhones[id_num].price : Accessories[id_num].price;
+          // setTotal( total + (cheak === 0) ?SmartPhones[id_num].price : Accessories[id_num].price );
+
+          return(
+            <>
+            <div className="cart-item" key={ind}>
+              <img src={(cheak === 0) ? SmartPhones[id_num].ImgAdd : Accessories[id_num].ImgAdd} alt="" />
+              <h2>{(cheak === 0) ? SmartPhones[id_num].FullName : Accessories[id_num].FullName}</h2>
+              <h3>{(cheak === 0) ?SmartPhones[id_num].price : Accessories[id_num].price}</h3>
+            </div>
+            <hr/>
+            </>
+          );
+        })
+      }
+      <hr />
+      <div className="total-buy">
+        <button onClick={() =>{
+          delAllItem();
+        }}>Buy Now</button>
+        {/* <h2>Total : {total}</h2> */}
+      </div>
+
+    </div>
+
     </>
   )
 }
--- src/pages/SmartPhone/SmartPhone_Details.jsx
+++ src/pages/SmartPhone/SmartPhone_Details.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './SmartPhon_Details.css';
 
 import Navbar from '../../Components/Navbar/Navbar';
@@ -6,15 +6,33 @@ import SmartPhones from "../../Utils/SmartPhones"
 import Accessories from "../../Utils/Accessories"
 import { useParams } from 'react-router-dom';
 
+// Retriving data from local sstorage
+const getLocalItem = () => {
+  let item = localStorage.getItem("Lists");
+  console.log( item );
+  if( item ){
+    return JSON.parse(localStorage.getItem("Lists"));
+  }
+  else{
+    return [];
+  }
+}
+
 const SmartPhone_Details = () => {
 
-  // const [list, setList ] = useEffect([]);
+  const [list, setList ] = useState(getLocalItem());
+  // console.log(list);
   const { id } = useParams();
+
+  // storing it to local Storage
+  useEffect(() => {
+    localStorage.setItem("Lists", JSON.stringify(list))
+  }, [list]);
 
   // console.log( SmartPhones[0].FullName );
   var cheak = id%10;
   var id_num = parseInt(id/10);
-  console.log( cheak, id_num ); 
+  // console.log( cheak, id_num ); 
 
   return (
     <>
@@ -22,7 +40,9 @@ const SmartPhone_Details = () => {
     <div className='Product_Details p1 m1'>
       <div className="left">
         <img src={(cheak === 0) ? SmartPhones[id_num].ImgAdd : Accessories[id_num].ImgAdd} alt="" />
-        <button><i className="fa-solid fa-cart-shopping"></i>Add To Cart </button>
+        <button onClick={() => {
+          setList([ ...list, id]);
+        }}><i className="fa-solid fa-cart-shopping"></i>Add To Cart </button>
       </div>
       <div className="right">
         <h1>{(cheak === 0) ? SmartPhones[id_num].FullName : Accessories[id_num].FullName}</h1>
